3092e176e2216b380a5e2cf2d398ca0492bbea34
geraldjahn <jahn310@gatech.edu>
2023-07-20 16:09:49 -0400

    Additional Tasks Included

--- Switch/switch-time.py
+++ Switch/switch-time.py
@@ -17,15 +17,23 @@ Creates a 3x3 matrix with independent dynamic queues
 '''
 packetSwitch = np.zeros((3,3))
 
-#lamb = float(sys.argv[1])   # arrival rate
-lamb = 1 / 6
-rho = 1 / 2   # traffic intensity
-mu = lamb / rho * 3       # service trial success rate -> here, 1 assumes that it dequeues every phase when the VOQ is not empty
+'''
+Independent Variable: Traffic Intensity
+* This simulation is setting the service rate to a constant value 1.
+
+Constraints:
+    1. the n x n constant matrix with the value lambda must be doubly substochastic. <=> λ <= 1 / 3
+    2. the traffic intensity and the derived service rate must have a value between [0, 1).
+'''
+
+rho = float(sys.argv[1])    # traffic intensity
+lamb = rho / 3              # arrival trial success rate
+mu = 1                      # service trial success rate -> here, 1 assumes that it dequeues every phase when the VOQ is not empty.
 
 N = 10000
 
 m = Munkres()
-queue = []
+actual_queue = []
 size = 0
 sample = []
 
@@ -44,7 +52,7 @@ for t in range(N):
                 size += 1
                 add += 1
 
-    print(f"{add} new jobs were added.")
+    print(f"\n{add} new jobs were added.")
     print("Current status of the switch is:")   # remove this later
     print(packetSwitch)
 
@@ -56,7 +64,7 @@ for t in range(N):
     '''
     if size > 0 and perform_bernoulli_trial(mu):
 
-        print("removal is proceeded.")
+        print("\nRemoval is proceeded.")
 
         # returns a list of tuples of the matrix's corrdinates
         maxWeight = m.compute((-1 * packetSwitch))
@@ -71,19 +79,27 @@ for t in range(N):
             if packetSwitch[remove[0]][remove[1]] > 0:  # edge case of removing from zeros.
                     packetSwitch[remove[0]][remove[1]] -= 1
                     size -= 1
-        print("The total weight of jobs chosen in Phase %d's schedule is %d." % (t, remWeight))
+        print(f"The total weight of jobs chosen in Phase {t}'s schedule is {remWeight}.")
+        # Hypothesis: W(t) --> λn
 
-    # Recording Process
-    queue.append(size)
-    if t > N / 2:
-        sample.append(size)
+        # Additional Task 1. Compute the total number of non-empty queues 
+        filled_queues = packetSwitch[np.where(packetSwitch > 0)]
+        print(f"There is/are {filled_queues.size} non-empty queue(s) in the switch in phase {t}.")
 
-mean = np.average(sample)
-    
-    
+        # 2. Compute the maximum sum between the maximum sum of columns and that of rows
+        # Hypothesis: C(t) --> ln(n)
+        row_sums = np.sum(packetSwitch, axis = 1)
+        col_sums = np.sum(packetSwitch, axis = 0)
+        max_sum = np.maximum(np.max(row_sums), np.max(col_sums))
+        print(f"The maximum axial sum recorded in the matrix in phase {t} is {max_sum}.")
+
+    # Recording Process --> Actual Queue Length
+    actual_queue.append(size)
+
 # Overview
+print(f"\nWhile the traffic intensity is {rho}, the average queue length for each VOQ is .")
 plt.title("Average Queue Length over Time")
-plt.plot(range(N), queue)
+plt.plot(range(N), actual_queue)
 plt.xlabel("t")
 plt.ylabel("μ[q(t)]")
 plt.show()
